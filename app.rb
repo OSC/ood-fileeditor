d01eedbe0a3f9e7260e81f77ae25cbd04c1c79a0
Eric Franz <efranz@osc.edu>
2017-09-18 18:06:57 -0400

    update comments

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
 require 'open3'
 
 # There are 11 data values for each `ps` item. Here, we create a Struct object to map
-#  each value to a named parameter.
+#  each value to an attribute on the Struct
 AppProcess = Struct.new(:user, :pid, :pct_cpu, :pct_mem, :vsz, :rss, :tty, :stat, :start, :time, :command)
 
 helpers do
-  # This command will parse a string output from the `ps` command and map it to
-  #  an array of AppProcess objects.
+  # Parse a string output from the `ps aux` command and return an array of
+  # AppProcess objects, one per process
   def parse_ps(ps_string)
     ps_string.split("\n").map { |line| AppProcess.new(*(line.split(" ", 11))) }
   end
@@ -14,12 +14,12 @@ end
 
 # Define a route at the root '/' of the app.
 get '/' do
-  # Define your variables that will be sent to the view.
+  # Define variables that will be available in the view
   @title = "Currently Running OnDemand Passenger Apps"
   @command = "ps aux | grep App | grep -v grep"
   @app_processes = []
 
-  # Run the command and capture the stdout, stderr, and exit code as separate variables.
+  # Run the command, and parse the output
   stdout_str, stderr_str, status = Open3.capture3(@command)
   if status.success?
     @app_processes = parse_ps(stdout_str)
