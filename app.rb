ae8e81b17b679f84185d0f221a3522c247f9068c
Tyler Harris <tharris@osc.edu>
2018-06-04 15:27:39 -0400

    Initial

--- app.rb
+++ app.rb
@@ -1,5 +1,4 @@
 require 'erubi'
-require './command'
 
 set :erb, :escape_html => true
 
@@ -8,25 +7,8 @@ if development?
   also_reload './command.rb'
 end
 
-helpers do
-  def dashboard_title
-    "Open OnDemand"
-  end
-
-  def dashboard_url
-    "/pun/sys/dashboard/"
-  end
-
-  def title
-    "Passenger App Processes"
-  end
-end
-
 # Define a route at the root '/' of the app.
 get '/' do
-  @command = Command.new
-  @processes, @error = @command.exec
-
   # Render the view
-  erb :index
+  erb :application
 end
