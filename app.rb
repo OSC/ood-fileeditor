791712d6784577c411bf10a742fa07d7ac383cf9
Tyler Harris <tharris@osc.edu>
2018-06-29 12:17:32 -0400

    Fix some requested changes

--- app.rb
+++ app.rb
@@ -1,38 +1,9 @@
 require 'sinatra'
-#require './fileserve.rb'
 require 'erb'
 require 'pathname'
-#require 'dir'
-#require 'ood-appkit'
 
-set :erb, :layout => :"layout.html" # use ERB as template engine and keeps the top bar
+set :erb, :layout => :layout # use ERB as template engine and keeps the top bar
 set :escape_html, true    # safety first!
-set :static, true   # in case I feel like making this modular, set this explicitly
-set :public_folder, './public'  # same thing
-set :views, './views'   # yeah
-
-# fileserve = FileServer.new
-# fileserve.run!
-# register Sinatra::FileAPI
-
-# get '/fs/*' do
-#   fileserve.call(env)
-# end
-
-# put '/fs/*' do
-#   fileserve.call(env)
-# end
-
-# before do
-#   @env = env
-# end
-
-# serve '/fs/*'
-
-# This might get dangerous, but it's the only way I can see to deliver the js files without Sinatra 404ing
-# get '/public/*' do
-#   send_file './public/' + params[:splat][0]
-# end
 
 helpers do
   def files_api_url(path)
@@ -52,11 +23,6 @@ end
 get '/edit' do
   redirect to('/edit/'), 301
 end
-
-# sends the file as plaintext
-# get '/file/*' do
-#   send_file '/' + params[:splat][0]
-# end
 
 get '/edit/*' do
 
@@ -83,25 +49,17 @@ get '/edit/*' do
   end
 
   # Render the view
-  erb :"edit.html"
-end
-
-get '/pages/index' do
-  erb :"index.html"
-end
-
-get '/pages/about' do
-  erb :"about.html"
+  erb :edit
 end
 
 not_found do
-  erb :"404.html"
+  erb 404.to_sym
 end
 
 error 500 do
-  erb :"500.html"
+  erb 404.to_sym
 end
 
 error 422 do
-  erb :"422.html"
+  erb 422.to_sym
 end
